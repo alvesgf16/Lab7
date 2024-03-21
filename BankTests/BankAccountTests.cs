f2e868e7f5080cd49b64a571186d37c11fac2a1e
Gabriel Windows <alvesgabriel1994@gmail.com>
2024-03-21 09:52:55 -0600

    test: verify debit amount less than zero

--- BankTests/BankAccountTests.cs
+++ BankTests/BankAccountTests.cs
@@ -25,4 +25,16 @@ public class BankAccountTests
         double actual = account.Balance;
         Assert.That(actual, Is.EqualTo(expected).Within(0.001), "Account not debited correctly");
     }
+
+    [Test]
+    public void Debit_WhenAmountIsLessThanZero_ShouldThrowArgumentOutOfRange()
+    {
+        // Arrange
+        double beginningBalance = 11.99;
+        double debitAmount = -100.00;
+        BankAccount account = new("Mr. Bryan Walton", beginningBalance);
+
+        // Act and assert
+        Assert.That(() => account.Debit(debitAmount), Throws.TypeOf<ArgumentOutOfRangeException>());
+    }
 }
